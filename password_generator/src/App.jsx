c69ae411905e141695e62b3bcb4a50137e6873c5
thiru49 <thiruppathi497@gmail.com>
2023-12-26 13:31:17 +0530

    useEffect,useCallback,useRef implement

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState({
+    length: '8',
+    numberAllowed: false,
+    charAllowed: false,
+    password: '',
+    
+  })
+  const passwordref = useRef(null)
+
+  const copyClipBoard = () =>{
+    window.navigator.clipboard.writeText(state.password);
+    passwordref.current.select()
+  }
+  const generatePassword = useCallback(()=>{
+      let pass = ''
+      let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+      if (state.numberAllowed) str+='0123456789';
+      if (state.charAllowed) str+='!@#$%^&*()-=_+[]{}|;:,.<>?';
+       
+      for(let i=0;i<state.length;i++){
+        const char = Math.floor(Math.random()* str.length);
+        pass+=str.charAt(char)
+      }
+      setState((pre)=>({
+        ...pre,
+        password:pass
+      }))
+  },[state.length,state.numberAllowed,state.charAllowed])
+
+  console.log(`${state.length}`);
+  
+
+  useEffect(()=>{
+    generatePassword()
+  },[state.length,state.numberAllowed,state.charAllowed])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+          type="text"
+          className='outline-none w-full py-1 px-3 '
+          value={state.password}
+          onChange={(e)=>setState((pre)=>({
+            ...pre,
+            password:e.target.value
+          }))}
+          placeholder='Password'
+          ref={passwordref}
+        />
+        <button onClick={copyClipBoard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+          copy
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='flex gap-2 text-sm'>
+      <div className='flex gap-x-2 items-center'>
+        <input
+          type="range"
+          min={6}
+          max={100}
+          value={state.length}
+          name=""
+          id=""
+          className='cursor-pointer'
+          onChange={(e)=>setState((pervious)=>({
+            ...pervious,
+            length:e.target.value
+          }))} // Provide a function for onChange
+        />
+        <label htmlFor="passwordLength">Length: {state.length}</label>
+      </div>
+      <div className='flex gap-x-2 items-center'>
+
+        <input type="checkbox" defaultChecked={state.numberAllowed}
+        onChange={()=>{
+          setState((pre)=>(
+            {
+              ...pre,
+              numberAllowed:!pre.numberAllowed
+            }
+          ))
+        }}/>
+        <label>Number</label>
+        <input type="checkbox" defaultChecked={state.charAllowed}
+        onChange={()=>{
+          setState((pre)=>(
+            {
+              ...pre,
+              charAllowed:!pre.charAllowed
+            }
+          ))
+        }}/>
+        <label>Charactor</label>
+      </div>
+      </div>
+     
+    </div>
   )
 }
 
-export default App
+export default App;
